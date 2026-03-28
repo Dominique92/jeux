67919b66fab2ce9b232f285288a848b7aab412cd
Dominique Cavailhez <2606683+Dominique92@users.noreply.github.com>
2026-03-28 08:21:31 +0100

    Après essai de gradians

--- land/index.js
+++ land/index.js
@@ -1,14 +1,16 @@
 // Définition du terrain
-const tailleMonde = 400,
-  tailleCaseX = 16, // Pixels// 16
+const dateLancement = Date.now(),
+  tailleMonde = 600,
+  tailleCaseX = 10, // Pixels// 16
   tailleCaseY = tailleCaseX * 0.866, // cos 30°
   nbCasesX = Math.round(tailleMonde / tailleCaseX),
   nbCasesY = Math.round(tailleMonde / tailleCaseY),
   rnd = Array(12).fill().map(() => Date.now() * Math.random() % 1), // tableau de valeurs aléatoires
   //inTerrain = (xy) => 0 <= xy[0] && xy[0] < nbCasesX && 0 <= xy[1] && xy[1] < nbCasesY,
   cases = [], // Valeurs associées à chaque case [x] [y]
-  terrainEl = document.getElementById('terrain'), // DOM d'affichage de la couleur des cases
-  chronoEl = document.getElementById('chrono');
+  terrainEl = document.getElementById('terrain'); // DOM d'affichage de la couleur des cases
+
+console.log(nbCasesX * nbCasesY + ' cellules'); //DCMM
 
 // Fonction aléatoire pour calculer les bosses du terrain
 function rndSin(marqueur, x, periode, min, max) {
@@ -21,13 +23,15 @@ function rndSin(marqueur, x, periode, min, max) {
 // Initialise le DOM du terrain, regroupé par lignes
 for (let y = 0; y < nbCasesY; y++) {
   terrainEl.insertAdjacentHTML('beforeend', '<div/>');
-  cases[y] = [];
+  /* eslint-disable-next-line no-multi-assign */
+  cases[y] = cases[-1] = cases[nbCasesY] = [];
 
   for (let x = 0; x < nbCasesX; x++) {
     // Un DIV pour chaque case position: absolute
     terrainEl.lastChild.insertAdjacentHTML('beforeend', '<div>');
     Object.assign(terrainEl.lastChild.lastChild.style, {
       width: tailleCaseX * 1.6 + 'px', // Facteur de recouvrement
+      //height: tailleCaseY   * 1.6   + 'px', // Facteur de recouvrement
       top: (tailleCaseY * y) + 'px', // Triangle équilatéral
       left: (tailleCaseX * (x + y % 2 / 2)) + 'px', // En quiconce
     });
@@ -37,7 +41,8 @@ for (let y = 0; y < nbCasesY; y++) {
       eau: 10,
       verdure: 50,
       altitude: 0,
-      gradiants: [],
+      directions: [],
+      //couleur: [],
     };
 
     // Ajout de 3 bosses
@@ -49,17 +54,69 @@ for (let y = 0; y < nbCasesY; y++) {
 }
 
 // Calcul de la couleur d'une case
-function rgbCase(c) {
+/*
+function rgbCase(c) {//TODO DELETE
   return [255, 192, 128].time(c.altitude / 256)
     .plus([0, 64, 0].time(c.verdure / 256))
     .plus([0, 0, 128].time(c.eau / 256))
     .borne(0, 255)
+    .round()
     .join(',');
 }
+
+function calculCouleur() {
+  for (let y = 0; y < nbCasesY; y++)     
+    for (let x = 0; x < nbCasesX; x++) 
+    cases[y][x].couleur=rgbCase(cases[y][x]) ;
+ }
+*/
+
+// Calcule la couleur des éléments du terrain
+function calculCouleur() {
+  for (let y = 0; y < nbCasesY; y++) {
+    for (let x = 0; x < nbCasesX; x++) {
+      const c = cases[y][x],
+        wwwwcv = [255 * Math.random(), 255 * Math.random(), 255 * Math.random()], //DCMM
+        cv = ([255, 192, 128].time(c.altitude / 256))
+        .plus([0, 64, 0].time(c.verdure / 256))
+        .plus([0, 0, 128].time(c.eau / 256)),
+        cc = cv.rgb();
+
+      c.couleur = {
+        v: cv, // Forme vectorielle pour calculs
+        m: cc,
+        g: cc,
+        hg: cc,
+        hd: cc, // Formes string pour affichage
+      };
+
+      if (x) {
+        c.couleur.g = c.couleur.v.plus(cases[y][x - 1].couleur.v).div(2).rgb();
+        if (y) {
+          c.couleur.hg = c.couleur.v.plus(cases[y - 1][x - y % 2].couleur.v).div(2).rgb();
+          c.couleur.hd = c.couleur.v.plus(cases[y - 1][x - 1 + y % 2].couleur.v).div(2).rgb();
+        }
+      }
+      //( cases[y][x-1]  .couleur.m  +  cases[y][x].couleur.m)/2  ;        
+      //cases[y][x].couleur.g=( cases[y][x-1]  .couleur.m  +  cases[y][x].couleur.m)/2  ;        
+      //c.couleur.hg=( cases[y-1][x-y % 2]  .couleur.m+c.couleur.m)/2  ;     
+      //c.couleur.hd=( cases[y-1][x-1+y % 2]  .couleur.m+c.couleur.m)/2  ;     
+    }
+    cases[y][-1] = cases[y][0];
+    cases[y][nbCasesX] = cases[y][nbCasesX - 1];
+  }
+  for (let x = 0; x < nbCasesX; x++) {
+    cases[-1][x] = cases[0][x];
+    cases[nbCasesY][x] = cases[nbCasesY - 1][x];
+  }
+}
+//console.log(cases); //DCMM
 
 // Colore les éléments du terrain
 function affiche() {
   const debut = Date.now();
+
+  calculCouleur();
 
   for (let y = 0; y < nbCasesY; y++) {
     const ligneEl = terrainEl.children[y],
@@ -70,28 +127,43 @@ function affiche() {
       dxAv = dxAp - 1; // Quinconce
 
     for (let x = 0; x < nbCasesX; x++) {
-      ligneEl.children[x].style.backgroundImage = 'radial-gradient(rgb(' + rgbCase(caseLigne[x]) + ') 42%, transparent 70%)';
+      //const c0=' red ';//+caseLigne[x].couleur.hd,
+      const c0 = caseLigne[x + 1].couleur.g; //' grey ';caseLigneAv[x+dxAp].couleur.hd;
 
+      const ccc = 'radial-gradient(' + caseLigne[x].couleur.m + ' 42%, transparent 72%)';
+      /*',conic-gradient(' + c0 + '30deg,' +
+          //OKcaseLigne[x + 1].couleur.g + '90deg,' +
+          //OKcaseLigne[x].couleur.g + '270deg,'+ 
+          c0 + '90deg,' +
+          caseLigne[x + 1].couleur.g + '150deg,' +
+          caseLigne[x].couleur.g + '210deg,' +
+          caseLigne[x].couleur.g + '270deg,' +
+          c0+ '330deg,' +
+          c0 + ')';*/
+      //console.log(ccc); //DCMM
+      ligneEl.children[x].style.backgroundImage = ccc;
+
+      // Calcul des directions
       if (0 < x && x < (nbCasesX - 1) && 0 < y && y < (nbCasesY - 1)) {
+        //TODO traiter les émanations hors terrain
         ['altitude']
         .forEach((f) => {
-          caseLigne[x].gradiants[f] = [
+          caseLigne[x].directions[f] = [
             caseLigne[x + 1][f] - caseLigne[x - 1][f],
             (caseLigneAv[x + dxAv][f] + caseLigneAv[x + dxAp][f] -
               caseLigneAp[x + dxAv][f] - caseLigneAp[x + dxAp][f]) / 2,
           ];
         });
 
-        /* TEST */
-        const mm = Math.round(caseLigne[x].gradiants.altitude.abs(), 10) / 2,
-          rr = Math.atan(caseLigne[x].gradiants.altitude[0] / caseLigne[x].gradiants.altitude[1]) + 1.57;
+        /* TEST
+        const mm = Math.round(caseLigne[x].directions.altitude.abs(), 10) / 2,
+          rr = Math.atan(caseLigne[x].directions.altitude[0] / caseLigne[x].directions.altitude[1]) + 1.57;
         ligneEl.children[x].innerHTML =
-          '<div style="transform:rotate(' + rr + 'rad);;font-size:' + mm + 'px" z-index=1000000>→</div>';
+          '<div style="transform:rotate(' + rr + 'rad);;font-size:' + mm + 'px" z-index=1000000>→</div>'; */
       }
     }
   }
-
-  chronoEl.innerHTML = Date.now() - debut;
+  console.log('affiche ' + (Date.now() - debut) + ' ms'); //DCMM
 }
 affiche();
 //setInterval(affiche, 200);
@@ -109,6 +181,7 @@ document.addEventListener('click', (evt) => {
   affiche();
 });
 
+console.log('Fin index.js ' + (Date.now() - dateLancement) + ' ms'); //DCMM
 
 ///////// TEST //////////
 /*
--- land/util.js
+++ land/util.js
@@ -8,6 +8,10 @@ Array.prototype.time = function(multiplicateur) {
   return this.map((valeur) => valeur * multiplicateur);
 };
 
+Array.prototype.div = function(diviseur) {
+  return this.map((valeur) => valeur / diviseur);
+};
+
 Array.prototype.borne = function(min, max) {
   return this.map((valeur) => Math.max(min, Math.min(valeur, max)));
 };
@@ -18,4 +22,12 @@ Number.prototype.borne = function(min, max) {
 
 Array.prototype.abs = function() {
   return this.reduce((r, v) => r + Math.abs(v), 0);
+};
+
+Array.prototype.round = function() {
+  return this.map((v) => Math.round(v));
+};
+
+Array.prototype.rgb = function() {
+  return 'rgb(' + (this.borne(0, 255).round().join(',')) + ')';
 };
