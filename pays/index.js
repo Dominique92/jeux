1b6b9efdf3a26fbca4ee2e96669304734709a16f
Dominique Cavailhez <2606683+Dominique92@users.noreply.github.com>
2026-07-18 21:33:19 +0200

    Coloration cases

--- pays/index.js
+++ pays/index.js
@@ -11,14 +11,14 @@ terrainEl.style.height = (nbCases * tailleCaseY) + 'px';
 
 function pointAtXY(xy) {
   return [
-    Math.floor((xy[0] - xy[1] / 2 - 0.5) + nbCases) % nbCases * tailleCaseX + 11,
-    Math.floor((xy[1] - 0.5) + nbCases) % nbCases * tailleCaseY + 11,
+    (xy[0] + xy[1] / 2) % nbCases * tailleCaseX,
+    xy[1] % nbCases * tailleCaseY,
   ];
 }
 
 function xyAtPoint(pxy) {
   const y = Math.floor(pxy[1] / tailleCaseY) % nbCases,
-    x = Math.floor(pxy[0] / tailleCaseX + y / 2) % nbCases;
+    x = Math.floor(pxy[0] / tailleCaseX - y / 2) % nbCases;
   return [x, y];
 }
 
@@ -72,8 +72,12 @@ for (let cx = 0; cx < nbCases; cx++) {
     el.classList.add('case-terrain');
     el.style.left = pos[0] + 'px';
     el.style.top = pos[1] + 'px';
+    el.style.backgroundColor = 'rgb(133, 83, 57)';
+
   }
 }
+
+//<div id="25,9" style="width: 17px; top: 74.909px; left: 248px; background-image: radial-gradient(rgb(133, 83, 57) 42%, transparent 80%);"></div>
 
 cases[3][3] = {
   a: 1,
